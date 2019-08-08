4435c209ad8a4b4686e5ba7921412d2c0bf63114
a1744546 <a1744546@f21141e3-08a5-453d-8ed1-dc214aa9aee8>
2019-08-08 05:29:26 +0000

    s

--- 2019/s2/oop/practical-02/function-1-3.cpp
+++ 2019/s2/oop/practical-02/function-1-3.cpp
@@ -5,7 +5,7 @@ int a =0, b = 0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0;
  	
  	for(int m =0;m<4;m++){
  		for(int z=0;z<4;z++){
- 		switch(array[i][z]){
+ 		switch(array[m][z]){
  			case 0:a++;
  			break;
  			case 1:b++;
