826b2a1f6ea867906a850b64757832d6b024ccae
a1744546 <a1744546@f21141e3-08a5-453d-8ed1-dc214aa9aee8>
2019-09-12 23:18:55 +0000

    2

--- 2019/s2/oop/practical-exam-03/Player.h
+++ 2019/s2/oop/practical-exam-03/Player.h
@@ -7,8 +7,8 @@ class Player{
 		int number;
 		string name;
 		Player();                  // a default constructor 
-		Player(int jNumber, string pName);    // a constructor that takes the jersey number and player name
-		string get_name();        // returns the name of the player
+		Player(int jNumber, std::string pName);    // a constructor that takes the jersey number and player name
+		std::string get_name();        // returns the name of the player
 		int get_jersey_number();  // returns the player's jersey number
 		~Player();                //A default destructor
 
--- 2019/s2/oop/practical-exam-03/main-1-1.cpp
+++ 2019/s2/oop/practical-exam-03/main-1-1.cpp
@@ -7,7 +7,7 @@ Player A;
 	cout << A.get_jersey_number()<< endl;
 	cout << A.get_name() << endl;
 
-	Player* B = new Player("test",5);
+	Player* B = new Player(5,"test");
 	cout << B->get_jersey_number()<< endl;
 	cout << B->get_name() << endl;
 delete[] B;
