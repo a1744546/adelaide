8249253a072ab50b20b2c58c1fba02f105897e92
a1744546 <a1744546@f21141e3-08a5-453d-8ed1-dc214aa9aee8>
2021-06-08 09:07:35 +0000

    1

--- 2021/s1/spc/assignment4/part1.c
+++ 2021/s1/spc/assignment4/part1.c
@@ -9,17 +9,17 @@ int main()
    
 
     pthread_create(&thid,NULL,slow_function2,NULL);
-    
+    int i,j,err;
     pthread_t child[10];
-    for(i = 0; i < 9; i++) {
+    for(i = 0; i < 10; i++) {
      err = pthread_create(&child[i], NULL, slow_function1,NULL);//for slow function1
      if(err) {
       exit(0);
      }
     }
     pthread_join(thid, NULL);
-    for(i = 0; i < 10; i++)
-    pthread_join(child[i], NULL);
+    for(j = 0; j < 10; j++)
+    pthread_join(child[j], NULL);
     return 0;
 
 }
