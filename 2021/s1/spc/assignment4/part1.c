6d6c5e2cbeb597a933d17d20618cf2ed93f1ee23
a1744546 <a1744546@f21141e3-08a5-453d-8ed1-dc214aa9aee8>
2021-06-09 05:44:59 +0000

    3

--- 2021/s1/spc/assignment4/part1.c
+++ 2021/s1/spc/assignment4/part1.c
@@ -5,21 +5,38 @@
 
 int main()
 {
-    pthread_t thid;//for slowfunc 2
+    pthread_t thid;//Creates a thread for slowfunc 2
    
 
-    pthread_create(&thid,NULL,slow_function2,NULL);
-    int i,j,err;
-    pthread_t child[10];
-    for(i = 0; i < 10; i++) {
-     err = pthread_create(&child[i], NULL, slow_function1,NULL);//for slow function1
-     if(err) {
-      exit(0);
-     }
+    if(pthread_create(&thid,NULL,slow_function2,NULL))
+    {
+        fprintf(stderr, "Error creating  thread for slowfunc2\n");
+        return 1;
     }
-    pthread_join(thid, NULL);
+    int i,j,err;
+    pthread_t child[10];//Creates a thread for slow function1
+    for(i = 0; i < 10; i++) {
+        if(pthread_create(&child[i], NULL, slow_function1, NULL))
+        {
+            fprintf(stderr, "Error creating  thread for slowfunc1\n");
+            return 1;
+        }
+    }
+    // Ensure Threads Join Well
+    if(pthread_join(thid, NULL))
+    {
+        fprintf(stderr, "Error joining thread for slowfunc2\n");
+        return 2;
+    }
     for(j = 0; j < 10; j++)
-    pthread_join(child[j], NULL);
+    {
+        if(pthread_join(child[j], NULL))
+        {
+            fprintf(stderr, "Error joining thread for slowfunc1\n");
+            return 2;
+        }
+        
+    }
     return 0;
 
 }
