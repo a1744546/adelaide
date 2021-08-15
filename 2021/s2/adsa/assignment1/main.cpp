eef5dc2cc98a216b1d791c255e39de94aa1cc4b2
a1744546 <a1744546@f21141e3-08a5-453d-8ed1-dc214aa9aee8>
2021-08-15 09:50:26 +0000

    as1

--- 2021/s2/adsa/assignment1/main.cpp
+++ 2021/s2/adsa/assignment1/main.cpp
@@ -1,23 +1,31 @@
+#include <string>
+#include <vector>
+using namespace std;
+#include <iostream>
+//this function is used to make two input same size
+//for example 100 and 4 will become 100 and 004 so the for loop in other function can run successful
 void samesize(string &s1,string &s2){
     int n;
     if(s1.size()>s2.size()){
-        for(int i=0;i < s1.size()-s2.size();i++){
-            b="0"+b;
+        n =s1.size()-s2.size();
+        for(int i=0;i<n;i++){
+            s2="0"+s2;
         }
     }
     if(s1.size()<s2.size()){
-        for(int i=0;i < s1.size()-s2.size();i++){
-            a="0"+a;
+        n =s2.size()-s1.size();
+        for(int i=0;i<n;i++){
+            s1="0"+s1;
         }
     }
 }
 
-string add(string s1, string s2, int c){
-    samesize(s1,s2);  //make input a b same size
+//add
+string add(string a, string b, int c){
+    samesize(a,b);  //make input a b same size
     string out="";
-    int o;
     int carry=0;
-    int size=s1.size();
+    int size=a.size();
     
     for(int i=size-1;i>=0;i--){
         int n1;
@@ -38,20 +46,18 @@ string add(string s1, string s2, int c){
             carry=0;
             if(n1+n2+1>=c){
                 carry=1;
-                int o=n1+n2+1-c;
-                out=to_string(o)+out;
+                out=to_string(n1+n2+1-c)+out;
             }else{
-                int o=n1+n2+1;
-                out=to_string(o)+out;
+                out=to_string(n1+n2+1)+out;
             }
         }else{
             if(n1+n2>=c){
                 carry=1;
-                int o=n1+n2-c;
-                out=to_string(o)+out;
+
+                out=to_string(n1+n2-c)+out;
             }else{
-                int o=n1+n2;
-                out=to_string(o)+out;
+
+                out=to_string(n1+n2)+out;
             }
         }
     }
@@ -62,12 +68,11 @@ string add(string s1, string s2, int c){
     return out;
 }
 
-// this funciton is use to calculate Subtraction
-string cal_sub(string a, string b, int c){
+//subtraction
+string sub(string a, string b, int c){
     samesize(a,b);  //make input a b same size
     string out="";
-    int o;
-    bool carry=false;
+    int carry=0;
     int size=a.size();
     
     for(int i=size-1;i>=0;i--){
@@ -85,89 +90,91 @@ string cal_sub(string a, string b, int c){
             n2=0;
         }
         
-        if(carry==true){
-            carry=false;
+        if(carry==1){
+            carry=0;
             if(n1-n2-1<0){
-                carry=true;
-                int o=n1+c-n2-1;
-                out=to_string(o)+out;
+                carry=1;
+ 
+                out=to_string(n1+c-n2-1)+out;
             }else{
-                int o=n1-n2-1;
-                out=to_string(o)+out;
+
+                out=to_string(n1-n2-1)+out;
             }
         }else{
             if(n1-n2<0){
-                carry=true;
-                int o=c+n1-n2;
-                out=to_string(o)+out;
+                carry=1;
+
+                out=to_string(c+n1-n2)+out;
             }else{
-                int o=n1-n2;
-                out=to_string(o)+out;
+
+                out=to_string(n1-n2)+out;
             }
         }
     }
     return out;
 }
 
-//this function is used to make shifting
-string MakeShifting(string str, int stepnum)
+//make shifting
+string shift(string str, int num)
 {
-    string shifted = str;
-    for (int i = 0 ; i < stepnum ; i++)
-        shifted = shifted + '0';
-    return shifted;
+    for (int i = 0 ; i < num ; i++)
+        str = str + '0';
+    return str;
 }
 
-//this funciont is used to calculate the multiply
-string multiply(string a, string b,int c){
+//multiply
+string mul(string a, string b,int c){
     samesize(a,b);
     int n=a.size();
-    
+    //1 digit mul
     if(n==1){
         int n1=stoi(a);
         int n2=stoi(b);
         int o = n1*n2;
-        int out=o;
-        if(out>c){
-            out= o%c+10*(o/c);
-        }else if(out==c){
-            out=10;
+        if(o>c){
+            o= o%c+10*(o/c);
+        }else if(o==c){
+            o=10;
         }
         
-        return to_string(out);
+        return to_string(o);
     }
     
     int fh = n/2;
     int sh = (n-fh);
-      
+    //first part
     string a0 = a.substr(0, fh);
     string a1 = a.substr(fh, sh);
-      
+    //second part
     string b0 = b.substr(0, fh);
     string b1 = b.substr(fh, sh);
 
-    string P1 = multiply(a0, b0,c);
-    string P2 = multiply(a1, b1,c);
-    string P3 = multiply(cal_plus(a0, a1,c), cal_plus(b0, b1,c),c);
-    
-    return cal_plus(cal_plus(MakeShifting(P1, 2*(n-n/2)),P2,c),MakeShifting(cal_sub(P3,cal_plus(P1,P2,c),c), n-(n/2)),c);
+    string P1 = mul(a0, b0,c);
+    string P2 = mul(a1, b1,c);
+    string P3 = mul(add(a0, a1,c), add(b0, b1,c),c);
+    return add(add(shift(P1, 2*(n-n/2)),P2,c),shift(sub(P3,add(P1,P2,c),c), n-(n/2)),c);
 }
+//delete the front 0
+string del0(string str){
+    while(str[0]=='0'){
+        if(str[0]=='0'){
+            str=str.substr(1, str.size()-1);
 
-
-int main(){
-    string a, b;
-    int c;
-    cin>>a>>b>>c;
-    
-    string o1=cal_plus(a,b,c); // calculate the sum of a b
-    string o3=multiply(a,b,c); // calculate the product of ab
-    int i =0;
-    while(o3[0]=='0'){ // use to delete the 0 in front 00100-> 100
-        if(o3[0]=='0'){
-            o3=o3.substr(1, o3.size()-1);
-            i++;
+            //cout<<str<<endl;
         }
     }
+
+    return str;
+}
+
+int main(){
+    string s1, s2;
+    int b;
+    cin>>s1>>s2>>b;
+    
+    string o1=add(s1,s2,b);
+    string o3=mul(s1,s2,b);
+    o3 = del0(o3);
     cout<<o1<<" "<<o3<<endl;
     
     return 0;
