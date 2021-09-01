90f4713fc1003d04c68a2ccedc697f6ce4f2dd86
a1744546 <a1744546@f21141e3-08a5-453d-8ed1-dc214aa9aee8>
2021-09-01 08:51:34 +0000

    workshop06-activity

--- 2021/s2/cs/workshop06/question07.asm
+++ 2021/s2/cs/workshop06/question07.asm
@@ -7,4 +7,26 @@
 	@numbers
 	@arr
 // add your instructions after this line
-
+@x
+M=0
+@sum
+M=0
+(loop)
+    @10
+    D=A
+    @x
+    D=M-D
+    @END
+    D;JGE
+    
+    @x
+    D=M
+    @SUM
+    M=M+D
+    @x
+    M+=1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
